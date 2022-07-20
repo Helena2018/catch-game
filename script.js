feed1042e39e0dfb13d3b87865df9e543723a983
Helena2018 <helena2020liu@gmail.com>
2022-07-20 17:19:36 +1200

    grab info from DOM

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+const game_container = document.getElementById('gmae-container')
 
 screens[0].classList.add('up')
